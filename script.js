2daafb7330f41b8a6adb8d999602c066386ab0bf
Ndamulelo <114615218+NdamuleloMulaudzi@users.noreply.github.com>
2024-08-05 16:45:25 +0200

    populate the display with input values from buttns

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+let firstNumber = 0;
+let secondNumber = 0;
+
+const inputDisplay = document.querySelector("#displayNumber");
+const container = document.querySelector("#container");
+const one = document.querySelector("#one");
+
 function add(num_1, num_2) {
   return num_1 + num_2;
 }
@@ -25,4 +32,55 @@ function operate(operator, num_1, num_2) {
   }
 }
 
+// one.addEventListener("click", () => {
+//   inputDisplay.value += 1;
+// });
 function display() {}
+
+container.addEventListener("click", (e) => {
+  let target = e.target;
+  switch (target.id) {
+    case "zero":
+      inputDisplay.value = 0;
+      break;
+    case "one":
+      inputDisplay.value = 1;
+      break;
+    case "two":
+      inputDisplay.value = 2;
+      break;
+    case "three":
+      inputDisplay.value = 3;
+      break;
+    case "four":
+      inputDisplay.value = 4;
+      break;
+    case "five":
+      inputDisplay.value = 5;
+      break;
+    case "six":
+      inputDisplay.value = 6;
+      break;
+    case "seven":
+      inputDisplay.value = 7;
+      break;
+    case "eight":
+      inputDisplay.value = 8;
+      break;
+    case "nine":
+      inputDisplay.value = 9;
+      break;
+    case "addition":
+      inputDisplay.value = "+";
+      break;
+    case "subraction":
+      inputDisplay.value = "-";
+      break;
+    case "multiplication":
+      inputDisplay.value = "*";
+      break;
+    case "division":
+      inputDisplay.value = "/";
+      break;
+  }
+});
